e44a2809dcc3c43cf48527a4cb1d05d4e7916220
lorrami,isabele,ana clara <isabeledocarmosantos@gmail.com>
2025-12-10 14:23:27 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import Card from './components/Card';
+
+const options = {
+  method: 'GET',
+  headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTIyMGU5MWJhNjlhOTNjYmY1YmQ1N2MyMDYxZjljMCIsIm5iZiI6MTcyMzE0MDE2OC4yMzY3ODEsInN1YiI6IjY2YWJlZDUzMWI2ZTIyNmMxMGExMzcyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nfIwiYY0em29_GsRaaEPA03jb765B0R0u_lNmNBEwGw'
+  }
+};
+
+const URL_FETCH = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc";
 
 const App = () => {
+
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const buscarFilmes = async () => {
+
+      try {
+        
+        const dados = await fetch(URL_FETCH, options).then((resposta) => resposta.json());
+        setFilmes(dados.results);
+        
+      } catch (error) {
+        console.log("Erro ao buscar filmes: ", error);
+      }
+
+    };
+
+    buscarFilmes();
+  }, []);
+
   return (
     <>
       <NavBar />
@@ -14,7 +47,9 @@ const App = () => {
           <h2 className="title">Filmes Populares:</h2>
 
           <div className="movies-container">
-            aqui vai os cards dos filmes...
+            {filmes.map((filme) => (
+              <Card key={filme.id} filme={filme} />
+            ))}
           </div>
 
         </div>
@@ -26,4 +61,3 @@ const App = () => {
 };
 
 export default App;
-
